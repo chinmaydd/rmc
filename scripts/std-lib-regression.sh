177e96a425cf128d6b19da925c01233fe2a7b9ef
Alexa VanHattum <alexavanhattum@gmail.com>
2021-08-19 13:11:37 +0000

    Rebase

--- scripts/std-lib-regression.sh
+++ scripts/std-lib-regression.sh
@@ -14,16 +14,18 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RMC_DIR=$SCRIPT_DIR/..
 
 # Log output
-STD_LIB_LOG="/tmp/SizeAndAlignOfDstTest/log.txt"
-
-# Use a unit test that requires mutex and cell
+STD_LIB_LOG="/tmp/StdLibTest/log.txt"
 echo "Starting RMC codegen for the Rust standard library"
 cd /tmp
-if [ -d SizeAndAlignOfDstTest ]; then rm -rf SizeAndAlignOfDstTest; fi
-cargo new SizeAndAlignOfDstTest
-cd SizeAndAlignOfDstTest
-cp $RMC_DIR/src/test/cbmc/SizeAndAlignOfDst/main_fail.rs src/main.rs 
-rustup component add rust-src --toolchain nightly > /dev/null 2>&1
+if [ -d StdLibTest ]; then rm -rf StdLibTest; fi
+cargo new StdLibTest
+cd StdLibTest
+
+# Check that we have the nighly toolchain, which is required for -Z build-std
+if ! rustup toolchain list | grep -q nightly; then
+  echo "Installing nightly toolchain"
+  rustup toolchain install nightly
+fi
 RUSTFLAGS="-Z trim-diagnostic-paths=no -Z codegen-backend=gotoc --cfg=rmc" RUSTC=rmc-rustc cargo +nightly build -Z build-std --target x86_64-unknown-linux-gnu 2> $STD_LIB_LOG
 
 # For now, we expect a linker error, but no modules should fail with a compiler
@@ -33,10 +35,15 @@ RUSTFLAGS="-Z trim-diagnostic-paths=no -Z codegen-backend=gotoc --cfg=rmc" RUSTC
 # removed to just allow the success of the previous line to determine the 
 # success of this script (with no $STD_LIB_LOG needed)
 RESULT=$?
-if grep -q "error: internal compiler error: unexpected panic" $STD_LIB_LOG; then
-  echo "Panic on building standard library"
+if grep -q "error: extern location for std does not exist:" $STD_LIB_LOG 
+then
+  echo "Successful RMC codegen for the Rust standard library."
+  echo "Expected linking failure: https://github.com/model-checking/rmc/issues/109,"
+elif $RESULT -eq 0
+then
+  echo "Successful RMC codegen for the Rust standard library."
+else 
+  echo "Unexpected panic on building standard library"
   cat $STD_LIB_LOG
   exit 1
-else 
-echo "Successful RMC codegen for the Rust standard library"
 fi
