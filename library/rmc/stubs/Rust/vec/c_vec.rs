f80dbc89ced3c00defe0f5ceab720959afdbbd72
Chinmay <chindesh@amazon.com>
2021-09-03 01:51:28 +0000

    Adds comments to NoBackVec

--- library/rmc/stubs/Rust/vec/c_vec.rs
+++ library/rmc/stubs/Rust/vec/c_vec.rs
@@ -6,7 +6,7 @@ use libc::{c_uint, size_t};
 
 // Abstraction which implements Vec operations using CBMC C primitives by only
 // implementing skeleton functions in Rust and exporting all core functionality
-// to C. 
+// to C.
 
 extern "C" {
     fn vec_new() -> *mut c_vec;
@@ -17,42 +17,42 @@ extern "C" {
     fn vec_pop(ptr: *mut c_vec) -> c_uint;
     fn vec_append(ptr1: *mut c_vec, ptr2: *mut c_vec);
     fn vec_insert(ptr: *mut c_vec, index: size_t, elem: c_uint);
+    fn vec_sized_grow(ptr: *mut c_vec, additional: size_t);
 }
 
-// A c_vec consists of a pointer to memory, the capacity of the allocation and 
+// TODO:
+// https://blog.eqrion.net/announcing-cbindgen/
+// how do we handle typesafety here ?
+// And must be identical to the definition in the .h file
+
+// A c_vec consists of a pointer to memory, the capacity of the allocation and
 // the length of the vector to be verified. This struture is used across
 // the exchange over the FFI boundary.
 #[repr(C)]
 pub struct c_vec {
     mem: *mut c_uint,
     len: size_t,
-    capacity: size_t
+    capacity: size_t,
 }
 
+// TODO: rephrase this
 // The Vec representation in Rust only tracks the pointer to the c_vec structure.
 pub struct Vec {
     ptr: *mut c_vec,
 }
 
+// explain what these are
 impl Vec {
     pub fn ptr(&mut self) -> *mut c_vec {
         return self.ptr;
     }
 
     pub fn new() -> Self {
-        unsafe {
-            Vec {
-                ptr: vec_new(),
-            }
-        }
+        unsafe { Vec { ptr: vec_new() } }
     }
 
     pub fn with_capacity(cap: usize) -> Self {
-        unsafe {
-            Vec {
-                ptr: vec_with_capacity(cap),
-            }
-        }
+        unsafe { Vec { ptr: vec_with_capacity(cap) } }
     }
 
     pub fn push(&mut self, elem: c_uint) {
@@ -62,13 +62,7 @@ impl Vec {
     }
 
     pub fn pop(&mut self) -> Option<c_uint> {
-        if self.len() == 0 {
-            None
-        } else {
-            unsafe {
-                Some(vec_pop(self.ptr))
-            }
-        }
+        if self.len() == 0 { None } else { unsafe { Some(vec_pop(self.ptr)) } }
     }
 
     pub fn append(&mut self, other: &mut Self) {
@@ -78,24 +72,27 @@ impl Vec {
     }
 
     pub fn capacity(&self) -> usize {
-        unsafe {
-            vec_cap(self.ptr) as usize
-        }
+        unsafe { vec_cap(self.ptr) as usize }
     }
 
     pub fn len(&self) -> usize {
+        unsafe { vec_len(self.ptr) as usize }
+    }
+
+    pub fn reserve(&mut self, additional: usize) {
         unsafe {
-            vec_len(self.ptr) as usize
+            vec_sized_grow(self.ptr);
         }
     }
 }
 
+// add documentation here
 #[cfg(abs_type = "c-ffi")]
 #[macro_export]
 macro_rules! rmc_vec {
   ( $val:expr ; $count:expr ) =>
     ({
-      let mut result = Vec::new();
+      let mut result = Vec::with_capacity($count);
       let mut i: usize = 0;
       while i < $count {
         result.push($val);
